2455f07ad302f8ae0de9b3ccbeebf6856607bcb6
takekaza <58417359+takekaza@users.noreply.github.com>
2020-01-13 19:38:12 +0900

    logOut-page-takei#1

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,4 +4,5 @@ class CardsController < ApplicationController
 
   def new
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "items#index"
   resources :cards, only: [:index, :new]
+  resources :items, only: :new
 end
