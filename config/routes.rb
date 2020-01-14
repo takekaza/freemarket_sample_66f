4179f5e3497fccd54d01a5c728951bdf9bb73157
JotaroYamamoto <kensin2131@gmail.com>
2020-01-14 21:11:59 +0900

    購入確認ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "cards#index"
   resources :cards, only: [:index, :new]
   resources :items, only: [:index, :new]
+  resources :categories, only:[:index]
 end
