58436b78cd67d010ec895287bfa8b514b2661ea4
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-01-15 11:01:25 +0900

    rake assets:precompile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_01_08_123205) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -37,7 +36,5 @@ ActiveRecord::Schema.define(version: 2020_01_08_123205) do
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-
 
 end
