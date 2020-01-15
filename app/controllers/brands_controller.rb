2501ef38a6930be35e500304c2fe5aa2dca15e54
takekaza <58417359+takekaza@users.noreply.github.com>
2020-01-15 16:13:08 +0900

    showitem-add-takei#1

---
+++ app/controllers/brands_controller.rb
@@ -0,0 +1,10 @@
+class BrandsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "items#index"
-  resources :cards, only: [:index, :new]
-  resources :items, only: :new
+  resources :cards, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :show]
+  resources :brands, only: [:index, :new]
+
 end
