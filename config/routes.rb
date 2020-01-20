eb3a9abfcc1312b85a84cff9bef2e221a810114f
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-01-20 19:26:14 +0900

    create_payjp

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "items#index"
+  get 'items/confirm'
+  get 'cards/card'
+  get 'cards/card2'
+
   resources :cards, only: [:index, :new, :create]
   resources :items, only: [:index, :new, :show]
   resources :brands, only: [:index, :new]
