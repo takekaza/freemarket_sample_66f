0c2b978ba7a301f2046986a4049f22d69fa46bc9
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-01-20 15:54:15 +0900

    add_confirm_haml

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "items#index"
+  root "items#confirm"
   resources :cards, only: [:index, :new, :create]
   resources :items, only: [:index, :new, :show]
   resources :brands, only: [:index, :new]
