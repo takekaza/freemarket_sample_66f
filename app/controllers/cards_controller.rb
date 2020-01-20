2df5233565fd1a8558ace86a06c3eda7c8f49e95
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-01-20 20:41:38 +0900

    change_haml_for_payjp

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -48,5 +48,49 @@ class CardsController < ApplicationController
     end
   end
 
+  def buy #クレジット購入
+
+    if card.blank?
+      redirect_to action: "new"
+      flash[:alert] = '購入にはクレジットカード登録が必要です'
+    else
+      @product = Product.find(params[:product_id])
+     # 購入した際の情報を元に引っ張ってくる
+      card = current_user.credit_card
+     # テーブル紐付けてるのでログインユーザーのクレジットカードを引っ張ってくる
+      Payjp.api_key = "sk_test_0e2eb234eabf724bfaa4e676"
+     # キーをセットする(環境変数においても良い)
+      Payjp::Charge.create(
+      amount: @product.price, #支払金額
+      customer: card.customer_id, #顧客ID
+      currency: 'jpy', #日本円
+      )
+     # ↑商品の金額をamountへ、cardの顧客idをcustomerへ、currencyをjpyへ入れる
+      if @product.update(status: 1, buyer_id: current_user.id)
+        flash[:notice] = '購入しました。'
+        redirect_to controller: "products", action: 'show'
+      else
+        flash[:alert] = '購入に失敗しました。'
+        redirect_to controller: "products", action: 'show'
+      end
+     #↑この辺はこちら側のテーブル設計どうりに色々しています
+    end
+  end
+
+  def delete
+    card = current_user.credit_card
+    if card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = 'sk_test_0e2eb234eabf724bfaa4e676'
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+     #ここでpay.jpの方を消している
+      card.delete
+     #ここでテーブルにあるcardデータを消している
+    end  
+
+  end
+
   
 end
