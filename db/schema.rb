c37dd720ce1078d33412ccb76eb438398c65c3c8
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-01-21 13:07:42 +0900

    update_schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2020_01_21_032632) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "card_id", null: false
+    t.string "customer_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "url"
     t.integer "item_id"
@@ -55,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_01_21_032632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
 end
