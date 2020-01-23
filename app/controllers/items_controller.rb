23debe516cb1884f09dadb7d0d484bf58be7d103
JotaroYamamoto <kensin2131@gmail.com>
2020-01-23 15:23:15 +0900

    修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, except: [:index,:new, :create,:show]
+  before_action :set_item, except: [:index,:new, :create]
   def index
     @item = Item.includes(:user)
     @image = Image.all   
