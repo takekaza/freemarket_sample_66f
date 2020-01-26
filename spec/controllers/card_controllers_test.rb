3af28579b558a6be7581da723d52f0fa59cc40bf
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-01-26 20:52:13 +0900

    render

---
+++ spec/controllers/card_controllers_test.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe CardsController do
+  describe 'GET #new' do
+    it "renders the :new template" do
+    end
+  end
+
+  describe 'GET #show' do
+  it "renders the :show template" do
+  end
+end
+
+end
