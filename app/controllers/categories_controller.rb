8662806d70a7f87e475a5765265be23a239e9209
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-01-28 19:11:32 +0900

    test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,7 @@
 class CategoriesController < ApplicationController
   def index
+    @images = Image.find_by(params[:id])
+    @category = Category.find(params[:id])
+    @item = Item.find(params[:id])
   end
 end
